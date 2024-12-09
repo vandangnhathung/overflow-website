ca2835b2b5c948dc617ede3b16eae3c19c9d266d
Adrian <contact@jsmastery.pro>
2024-12-09 16:08:27 +0100

    Implement Edit and Get Server Actions and Implemention

---
+++ app/(root)/questions/[id]/edit/page.tsx
@@ -0,0 +1,29 @@
+import { notFound, redirect } from "next/navigation";
+import React from "react";
+
+import { auth } from "@/auth";
+import QuestionForm from "@/components/forms/QuestionForm";
+import { getQuestion } from "@/lib/actions/question.action";
+import ROUTES from "@/constants/routes";
+
+const EditQuestion = async ({ params }: RouteParams) => {
+  const { id } = await params;
+  if (!id) return notFound();
+
+  const session = await auth();
+  if (!session) return redirect("/sign-in");
+
+  const { data: question, success } = await getQuestion({ questionId: id });
+  if (!success) return notFound();
+
+  if (question?.author.toString() !== session?.user?.id)
+    redirect(ROUTES.QUESTION(id));
+
+  return (
+    <main>
+      <QuestionForm question={question} isEdit />
+    </main>
+  );
+};
+
+export default EditQuestion;
--- components/forms/QuestionForm.tsx
+++ components/forms/QuestionForm.tsx
@@ -11,7 +11,7 @@ import { z } from "zod";
 
 import ROUTES from "@/constants/routes";
 import { toast } from "@/hooks/use-toast";
-import { createQuestion } from "@/lib/actions/question.action";
+import { createQuestion, editQuestion } from "@/lib/actions/question.action";
 import { AskQuestionSchema } from "@/lib/validations";
 
 import TagCard from "../cards/TagCard";
@@ -31,7 +31,12 @@ const Editor = dynamic(() => import("@/components/editor"), {
   ssr: false,
 });
 
-const QuestionForm = () => {
+interface Params {
+  question?: Question;
+  isEdit?: boolean;
+}
+
+const QuestionForm = ({ question, isEdit = false }: Params) => {
   const router = useRouter();
   const editorRef = useRef<MDXEditorMethods>(null);
   const [isPending, startTransition] = useTransition();
@@ -39,9 +44,9 @@ const QuestionForm = () => {
   const form = useForm<z.infer<typeof AskQuestionSchema>>({
     resolver: zodResolver(AskQuestionSchema),
     defaultValues: {
-      title: "",
-      content: "",
-      tags: [],
+      title: question?.title || "",
+      content: question?.content || "",
+      tags: question?.tags.map((tag) => tag.name) || [],
     },
   });
 
@@ -89,6 +94,30 @@ const QuestionForm = () => {
     data: z.infer<typeof AskQuestionSchema>
   ) => {
     startTransition(async () => {
+      if (isEdit && question) {
+        const result = await editQuestion({
+          questionId: question?._id,
+          ...data,
+        });
+
+        if (result.success) {
+          toast({
+            title: "Success",
+            description: "Question updated successfully",
+          });
+
+          if (result.data) router.push(ROUTES.QUESTION(result.data._id));
+        } else {
+          toast({
+            title: `Error ${result.status}`,
+            description: result.error?.message || "Something went wrong",
+            variant: "destructive",
+          });
+        }
+
+        return;
+      }
+
       const result = await createQuestion(data);
 
       if (result.success) {
@@ -213,7 +242,7 @@ const QuestionForm = () => {
                 <span>Submitting</span>
               </>
             ) : (
-              <>Ask A Question</>
+              <>{isEdit ? "Edit" : "Ask a Question"}</>
             )}
           </Button>
         </div>
